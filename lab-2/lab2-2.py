f5fd37200e9e58f360b9deab6d33cbc299dacfc5
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2024-01-12 00:46:30 +0700

    add new python file

--- lab-2/lab2-2.py
+++ lab-2/lab2-2.py
@@ -2,31 +2,36 @@ import RPi.GPIO as GPIO
 import spidev
 import time
 
-# GPIO Setup
+# ------------------------- GPIO Setup ---------------------------------
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT) # Red LED
 GPIO.setup(3, GPIO.OUT) # Green LED
-PWM = GPIO.PWM(3, 100) # PWM for green led
+pwm = GPIO.PWM(3, 100) # PWM for green led
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1000000
 
+#---------------------------- Loop -------------------------------------
 while True:
-    ch1 = 0
-    raw1 = spi.xfer2([1, (ch1 << 4) | 0x80, 0])
+    # Channel 1 light dim
+    channel1 = 0
+    raw1 = spi.xfer2([1, (channel1 << 4) | 0x80, 0])
     data1 = ((raw1[1]&3) << 8) | raw1[2]
-
-    ch2 = 1
-    raw2 = spi.xfer2([1, (ch2 << 4) | 0x80, 0])
+    
+    # Channel 2 Potentiometer
+    channel2 = 1
+    raw2 = spi.xfer2([1, (channel2 << 4) | 0x80, 0])
     data2 = ((raw2[1] & 3) << 8) | raw2[2]
 
     cycle = (data2 / 1024) * 100
-    PWM.start(cycle)
+    pwm.start(cycle)
 
-    if data1 > 1010:
+    # Make red LED turn on if light is off
+    print(data1)
+    if data1 > 1020:
         GPIO.output(2, GPIO.HIGH)
-    elif data1 < 990:
+    elif data1 < 1015:
         GPIO.output(2, GPIO.LOW)
 
     time.sleep(1)
